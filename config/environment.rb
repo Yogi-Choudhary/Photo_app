5bad75b9358d4f7aaf67550e1863c804436b6a5c
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-05-05 14:05:18 +0530

    setup_email_functionality_for_sign-up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+
+    :address => "smtp-relay.sendinblue.com",
+    :port => 587,
+    :user_name => ENV['send_in_blue_user_name'],
+    :password => ENV['send_in_blue_password'],
+    :authentication => "login",
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
