9b8fda8bda9ebc01b13fc7233fcc9ebfc69b66fd
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-05-05 22:44:30 +0530

    remove_turbolinks_and_fix_signup_bug

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,10 +12,19 @@ class Payment < ApplicationRecord
 
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
+    # customer = Stripe::Customer.create email: email,
+    #                                source: token
+
 
     Stripe::Charge.create customer: customer.id, 
                           amount: 1000, 
                           description: 'Premium', 
-                          currency: 'usd'
+                          currency: 'IND'
+
+    # Stripe::Charge.create customer: customer.id,
+    #                   amount: 1000,
+    #                   description: 'Premium', 
+    #                   currency: 'usd'
+    
   end
 end
